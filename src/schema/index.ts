b7bc4c3c8d10cef02111cecd138d349e32db06b6
Kudakwashe Mupeni <terrencemupeni@gmail.com>
2020-04-02 10:52:33 +0200

    feat 🎸: import schema

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -1,59 +1,7 @@
+import 'graphql-import-node'
 import { makeExecutableSchema } from 'graphql-tools'
 import resolvers from './resolvers'
-
-const typeDefs = `
-type User {
-  email: String!
-  id: ID!
-  name: String
-  posts: [Post!]!
-}
-
-type Post {
-  author: User
-  content: String
-  id: ID!
-  published: Boolean!
-  title: String!
-}
-
-type Query {
-  me: User
-  feed: [Post!]!
-  filterPosts(searchString: String): [Post!]!
-  post(where: PostWhereUniqueInput!): Post
-}
-
-type Mutation {
-  createDraft(authorEmail: String, content: String, title: String!): Post!
-  deleteOnePost(where: PostWhereUniqueInput!): Post
-  publish(id: ID): Post
-  signupUser(data: UserCreateInput!): User!
-}
-
-input PostWhereUniqueInput {
-  id: ID
-}
-
-input UserCreateInput {
-  email: String!
-  id: ID
-  name: String
-  posts: PostCreateManyWithoutPostsInput
-}
-
-input PostCreateManyWithoutPostsInput {
-  connect: [PostWhereUniqueInput!]
-  create: [PostCreateWithoutAuthorInput!]
-}
-
-input PostCreateWithoutAuthorInput {
-  content: String
-  id: ID
-  published: Boolean
-  title: String!
-}
-`
+import * as typeDefs from './schema.graphql'
 
 export const schema = makeExecutableSchema({
   resolvers,
