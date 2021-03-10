f44ad05c4c7c74c12db010948457359a196a0069
Kudakwashe Mupeni <terrencemupeni@gmail.com>
2021-03-10 21:26:28 +0200

    feat: add semantic release

---
+++ release.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    [
+      '@semantic-release/github',
+      {
+        assets: 'release/*.tgz',
+      },
+    ],
+    '@semantic-release/git',
+  ],
+  branches: ['main'],
+  preset: 'angular',
+}
