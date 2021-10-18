2d8e4eca6b9692e102cc5b1bf4089347e8b9d631
Kudakwashe Mupeni <terrencemupeni@gmail.com>
2021-10-18 11:42:24 +0200

    feat: refactor update post mutation

--- src/schema/resolvers/Post/Mutation/updatePost.ts
+++ src/schema/resolvers/Post/Mutation/updatePost.ts
@@ -1,30 +1,39 @@
 import { ApolloError } from 'apollo-server'
 import { MutationUpdatePostArgs } from '../../../../generated'
-import { Context } from '../../../../utils'
+import { Context, formatError } from '../../../../utils'
 
 export default async (
   _parent: unknown,
   { input }: MutationUpdatePostArgs,
   { prisma, userId }: Context,
 ) => {
-  const { title, content, id } = input
+  try {
+    const { title, content, id } = input
 
-  // check if post linked to this user exists
-  const postExists = await prisma.post.findFirst({
-    where: {
-      AND: [{ id }, { authorId: userId }],
-    },
-  })
+    if (!id || !userId) {
+      throw new ApolloError('Invalid input')
+    }
 
-  if (!postExists) {
-    throw new ApolloError('Cannot update post')
+    // check if post linked to this user exists
+    const postExists = await prisma.post.findFirst({
+      where: {
+        AND: [{ id }, { author: { id: userId } }],
+      },
+    })
+
+    if (!postExists) {
+      throw new ApolloError('Cannot update post')
+    }
+
+    return prisma.post.update({
+      where: { id },
+      data: {
+        ...(title && { title }),
+        ...(content && { content }),
+      },
+    })
+  } catch (error) {
+    formatError('updatePost', error)
+    return error
   }
-
-  return prisma.post.update({
-    where: { id },
-    data: {
-      ...(title && { title }),
-      ...(content && { content }),
-    },
-  })
 }
