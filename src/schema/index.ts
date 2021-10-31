5bcbf6c23d342fe6f522531dc665a04a79be056b
Kudakwashe Mupeni <terrencemupeni@gmail.com>
2021-10-31 20:14:19 +0200

    feat: update permission import

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -1,6 +1,6 @@
 import { makeExecutableSchema } from '@graphql-tools/schema'
 import { applyMiddleware } from 'graphql-middleware'
-import { permissions } from './permissions'
+import permissions from './permissions'
 import resolvers from './resolvers'
 import typeDefs from './schema.graphql'
 
